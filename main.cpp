f8831df811e9a35f5d872776e26ebaef42eb0828
Yury <yury_alekseev@yahoo.com>
2016-05-03 19:22:51 +0300

    Clean up some code.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include <vector>
 
 int main() {
-    std::vector<int> a = { 1, 2, 3, 4, 1};
-    std::vector<int> b = { 3, 4, 1, 2, 1, 3};
+    std::vector<int> a = { 1, 2, 3, 4, 1 };
+    std::vector<int> b = { 3, 4, 1, 2, 1, 3 };
     std::cout << util::longest_common_subsequence(a.begin(), a.end(), b.begin(), b.end()) << std::endl;
 
     return 0;
